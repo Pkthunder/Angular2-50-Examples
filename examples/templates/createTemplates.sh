7df0a543b3e8aa1f7493478b1579503a6c997132
Jared Perreault <jared_perreault@yahoo.com>
2015-07-11 15:19:27 -0400

    Added example templates

---
+++ examples/templates/createTemplates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+count=1
+url='https://raw.githubusercontent.com/curran/screencasts/gh-pages/introToAngular/examples/snapshots/snapshot'
+urlEnd='/index.html'
+urlFinal=''
+$name=''
+
+while [ $count -lt 51 ]; do
+	echo $count:
+	if [ $count -lt 10 ]; then
+		urlFinal=$url'0'$count$urlEnd
+		name='0'$count
+	else
+		urlFinal=$url$count$urlEnd
+		name=$count
+	fi
+	#echo $urlFinal
+	mkdir ex$name
+	cd ex$name
+	wget $urlFinal
+	cp index.html ex$name.html
+	rm index.html
+	cd ..
+	echo Done...
+	let count+=1
+done
+
