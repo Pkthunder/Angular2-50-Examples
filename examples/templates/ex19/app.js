540395f8b11079c6cc1eb937bda55ca53479eeb7
Jared Perreault <jared_perreault@yahoo.com>
2015-08-02 23:51:51 -0400

    Finally got Http to work

--- examples/templates/ex19/app.js
+++ examples/templates/ex19/app.js
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/angular2/angular2.d.ts" />
+/// <reference path="../../../typings/angular2/angular2.d.ts" />
 if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
     switch (arguments.length) {
@@ -11,41 +11,55 @@ if (typeof __metadata !== "function") __metadata = function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
-var fetch_1 = require('../../utils/fetch');
-var fetchJson = (function () {
-    function fetchJson() {
-    }
-    fetchJson.prototype.getJson = function () {
-        return window.fetch('../static/countries.json', {
-            method: 'GET',
-            headers: {
-                'Accept': 'appplication/json',
-            }
-        }).then(fetch_1.status).then(fetch_1.json);
-    };
-    return fetchJson;
-})();
-exports.fetchJson = fetchJson;
+var angular2_2 = require('angular2/angular2');
 var AppComponent = (function () {
-    function AppComponent($http) {
-        var _this = this;
+    function AppComponent(http) {
+        this.http = http;
         this.countries = [
             { "name": "China", "population": 1359821000 },
             { "name": "India", "population": 1205625000 },
             { "name": "United States of America", "population": 312247000 }
         ];
-        this.people = $http.getJson().then(function (response) {
-            _this.people = response;
-        });
+        this.people = 'hi';
+        console.log(this.people);
+        this.getData();
         console.log(this.people);
     }
+    /*getData() {
+        this.http.get('../../static/countries.json'), {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).
+        toRx().
+        map(res => res.json()).
+        subscribe(
+            data => this.onSuccess(data),
+
+            err => this.onError(err)
+        );
+    }*/
+    AppComponent.prototype.getData = function () {
+        var _this = this;
+        this.http.get('countries.json')
+            .toRx()
+            .map(function (res) { return res.json(); })
+            .subscribe(function (data) { return _this.onSuccess(data); }, function (err) { return _this.onError(err); });
+    };
+    AppComponent.prototype.onSuccess = function (data) {
+        console.log('data', data);
+    };
+    AppComponent.prototype.onError = function (err) {
+        console.log(err);
+    };
     AppComponent = __decorate([
-        angular2_1.Component({ selector: 'my-app', injectables: [fetchJson] }),
+        angular2_1.Component({ selector: 'my-app', viewInjector: [angular2_2.httpInjectables] }),
         angular2_1.View({
             templateUrl: './templates/ex19/ex19.html',
             directives: [angular2_1.NgFor]
         }), 
-        __metadata('design:paramtypes', [fetchJson])
+        __metadata('design:paramtypes', [angular2_2.Http])
     ], AppComponent);
     return AppComponent;
 })();
--- examples/templates/ex19/app.ts
+++ examples/templates/ex19/app.ts
@@ -1,21 +1,10 @@
-/// <reference path="../../typings/angular2/angular2.d.ts" />
+/// <reference path="../../../typings/angular2/angular2.d.ts" />
 
 import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
-import {status, json} from '../../utils/fetch';
 
-export class fetchJson {
+import {Http, httpInjectables} from 'angular2/angular2';
 
-	getJson() {
-		return window.fetch('../static/countries.json', {
-			method: 'GET',
-			headers: {
-				'Accept': 'appplication/json',
-			}
-		}).then(status).then(json);
-	}
-}
-
-@Component({selector: 'my-app', injectables: [fetchJson]})
+@Component({selector: 'my-app', viewInjector: [httpInjectables]})
 @View({ 
   templateUrl: './templates/ex19/ex19.html',
   directives: [NgFor]
@@ -25,18 +14,49 @@ export class AppComponent {
 	countries: Array<Object>;
 	people: any;
 
-	constructor($http: fetchJson) {
+	constructor(public http: Http) {
 		this.countries = [
 			{"name": "China", "population": 1359821000},
 	      	{"name": "India", "population": 1205625000},
 	      	{"name": "United States of America","population": 312247000}
 		];
-		this.people = $http.getJson().then((response) => {
-			this.people = response;
-		});
+		this.people = 'hi';
+		console.log(this.people);
+		this.getData();
 		console.log(this.people);
 	}
+		
+	/*getData() {
+		this.http.get('../../static/countries.json'), {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}).
+		toRx().
+		map(res => res.json()).
+		subscribe(
+			data => this.onSuccess(data),
 
+			err => this.onError(err)
+		);
+	}*/
+	getData() {
+		this.http.get('countries.json')
+			.toRx()
+			.map(res => res.json())
+			.subscribe(data => this.onSuccess(data),
+						err => this.onError(err)
+			);
+	}
+
+	onSuccess(data) {
+		console.log('data', data);
+	}
+
+	onError(err) {
+		console.log(err);
+	}
 }
 
 bootstrap(AppComponent);
